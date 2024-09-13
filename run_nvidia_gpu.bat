b50c232aa018b49ce76654fe90dbe545c994e747
syaofox <syaofox@gmail.com>
2024-09-13 15:47:41 +0800

    update:启动脚本添加MODELSCOPE_CACHE

--- run_nvidia_gpu.bat
+++ run_nvidia_gpu.bat
@@ -1,6 +1,8 @@
-set HF_ENDPOINT=https://hf-mirror.com
-set HF_HOME=%CD%\hf_download
+@REM set HF_ENDPOINT=https://hf-mirror.com
+set HF_HOME=%CD%\.cache\hf_download
+set MODELSCOPE_CACHE=%CD%\.cache\modelscope
 set U2NET_HOME=%CD%\models\u2net
+
 
 call conda activate comfyui
 python main.py
