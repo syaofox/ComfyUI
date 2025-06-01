092bce5572498655a62f9afaf1583c960de3f05a
syaofox <syaofox@gmail.com>
2025-06-01 23:38:48 +0800

    更新：节点改名对应修改，以及启动gui脚本

--- tools/cores/swapface_hyperlora_core.py
+++ tools/cores/swapface_hyperlora_core.py
@@ -31,43 +31,43 @@ def run(char: str, input_file: str, output_file: str, sub_body: bool = True,
             
         queue.watch_display(False)
         with Workflow():
-            analysis_models = FaceAnalysisModels('insightface', 'CUDA')
+            analysis_models = SFFaceAnalysisModels('insightface', 'CUDA')
             image, _ = LoadImageFromPath(input_file)
-            image, _, _, _, _ = ImageScalerByPixels(image, 'lanczos', 2.8000000000000007, True, None)
-            image, _, inverse_rotation_angle = AlignImageByFace(analysis_models, image, True, False, None)
-            bounding_infos, crop_images, _ = FaceCutout(analysis_models, image, 0, 0.4000000000000001, 'sdxl', 1, 12, 0, 10)
-            _, _, _, _, crop_image, bounding_info = ExtractBoundingBox(bounding_infos, crop_images, 0)
+            image, _, _, _, _ = SFImageScalerByPixels(image, 'lanczos', 2.8000000000000007, True, None)
+            image, _, inverse_rotation_angle = SFAlignImageByFace(analysis_models, image, True, False, None)
+            bounding_infos, crop_images, _ = SFFaceCutout(analysis_models, image, 0, 0.4000000000000001, 'sdxl', 1, 12, 0, 10)
+            _, _, _, _, crop_image, bounding_info = SFExtractBoundingBox(bounding_infos, crop_images, 0)
             instantid = InstantIDModelLoader('ip-adapter.bin')
             faceanalysis = InstantIDFaceAnalysis('CUDA')
             control_net = ControlNetLoader(r'instantid\diffusion_pytorch_model.safetensors')
             lora, image2 = HyperLoRALoadCharLoRA(char)
             model, clip, vae = CheckpointLoaderSimple(r'xl\dreamshaperXL_v21TurboDPMSDE.safetensors')
             model = HyperLoRAApplyLoRA(model, lora, 0.85)
-            clip2 = CLIPSetLastLayer(clip, -2)
+            clip2 = CLIPSetLastLayer(clip, -1)
             conditioning = BNKCLIPTextEncodeAdvanced('fcsks fxhks fhyks,a beautiful girl, Look at the camera, Real photography, 4K, RAW photo, close-up, exquisite makeup, delicate skin,  real photos, best picture quality, high details', clip2, 'length+mean', 'A1111')
             conditioning2 = BNKCLIPTextEncodeAdvanced('finger,hande,lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, bad feet', clip2, 'length+mean', 'A1111')
             model, positive, negative = ApplyInstantIDAdvanced(instantid, faceanalysis, control_net, image2, model, conditioning, conditioning2, 0, 0.6000000000000001, 0, 1, 0, 'average', crop_image, None)
             latent = VAEEncode(crop_image, vae)
             latent = KSamplerAdvanced(model, True, 100006, 4, 1.4000000000000001, 'dpmpp_sde', 'karras', positive, negative, latent, 1, 2, False)
             image3 = VAEDecode(latent, vae)
-            warped_image = FaceMorph(crop_image, image3, 'OUTLINE', 'Landmarks', 'CPU')
+            warped_image = SFFaceMorph(crop_image, image3, 'OUTLINE', 'Landmarks', 'CPU')
             latent2 = VAEEncode(warped_image, vae)
-            segmenter = PersonSegmenterLoader()
+            segmenter = SFPersonSegmenterLoader()
             if sub_body:
                 
-                occluder = OccluderLoader('xseg_3')
-                mask, _, _ = GeneratePreciseFaceMask(occluder, warped_image, 0.1, 0, 0, False, 0, False)
-                mask, _ = MaskChange(mask, -2, 0, False, 2, False)
-                segmenter = PersonSegmenterLoader()
-                masks = PersonMaskGenerator(segmenter, warped_image, False, False, False, True, False, 0.10000000000000002, True)
+                occluder = SFOccluderLoader('xseg_3')
+                mask, _, _ = SFGeneratePreciseFaceMask(occluder, warped_image, 0.1, 0, 0, False, 0, False)
+                mask, _ = SFMaskChange(mask, -2, 0, False, 2, False)
+                segmenter = SFPersonSegmenterLoader()
+                masks = SFPersonMaskGenerator(segmenter, warped_image, False, False, False, True, False, 0.10000000000000002, True)
                 mask = MaskComposite(mask, masks, 0, 0, 'subtract')
                 steps = [7,3]
                 print('去除身体, steps:', steps)
 
             else:       
                 
-                segmenter = PersonSegmenterLoader()
-                mask = PersonMaskGenerator(segmenter, warped_image, True, False, True, False, False, 0.10000000000000002, True)
+                segmenter = SFPersonSegmenterLoader()
+                mask = SFPersonMaskGenerator(segmenter, warped_image, True, False, True, False, False, 0.10000000000000002, True)
                 steps = [8,4]
                 print('不去除身体, steps:', steps)
                 
@@ -75,10 +75,10 @@ def run(char: str, input_file: str, output_file: str, sub_body: bool = True,
             latent2 = SetLatentNoiseMask(latent2, mask)
             latent2 = KSamplerAdvanced(model, True, 100008, steps[0], 1.2, 'dpmpp_sde', 'karras', positive, negative, latent2, steps[1], steps[0], False)
             image4 = VAEDecode(latent2, vae)
-            image5 = ImageColorMatch(image4, image4, 'RGB', 1, 'auto', 0, None)
-            image6, _ = FacePaste(bounding_info, image5, image)
-            image6 = ImageRotate(image6, inverse_rotation_angle, True) # type: ignore
-            image6 = TrimImageBorders(image6, 10) # type: ignore
+            image5 = SFImageColorMatch(image4, image4, 'RGB', 1, 'auto', 0, None)
+            image6, _ = SFFacePaste(bounding_info, image5, image)
+            image6 = SFImageRotate(image6, inverse_rotation_angle, True) # type: ignore
+            image6 = SFTrimImageBorders(image6, 10) # type: ignore
             images = util.get_images(image6)  # type: ignore
             images[0].save(output_file) # type: ignore
             
