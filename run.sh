22cb1ed61d05870508ad09f021f651baaedc04a1
syaofox <syaofox@gmail.com>
2025-09-11 07:09:37 +0800

    fix: ensure correct working directory before running main.py

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ export HF_HOME="$PWD/.cache/hf_download"
 export MODELSCOPE_CACHE="$PWD/.cache/modelscope"
 export U2NET_HOME="$PWD/models/u2net"
 
+cd /home/syaofox/data/dev/ComfyUI
+
 # 使用 uv 运行 main.py
 source .venv/bin/activate
 python main.py
